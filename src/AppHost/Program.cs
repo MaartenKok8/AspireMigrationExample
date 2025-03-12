6ab0834e4c3c19ed8be58d9b64207fca441de143
Maarten Kok <maarten.kok8@gmail.com>
2025-03-12 19:40:17 +0100

    Transfer Catalog Service app settings.

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ApiGateway>("apigateway");
 
-builder.AddProject<Projects.CatalogService>("catalogservice");
+builder.AddProject<Projects.CatalogService>("catalogservice")
+    .WithEnvironment("Dapr__PubSub", "pub-sub")
+    .WithEnvironment("Dapr__StateStore", "state-store");
 
 builder.Build().Run();
